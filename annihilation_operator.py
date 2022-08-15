146ccce7de70703ad87b7dda059260369275e987
christinadanielphysics <53619458+christinadanielphysics@users.noreply.github.com>
2022-08-15 11:22:29 -0400

    Working on the apply function for the annihilation operator

--- annihilation_operator.py
+++ annihilation_operator.py
@@ -1,11 +1,31 @@
+import occupation_state
+
 class Annihilation_Operator:
     def __init__(self,numerical_index,spin):
         self.numerical_index = numerical_index 
         self.spin = spin 
     def apply(self,simplified_occupation_state):
+        
+        initial_up_spin_list = simplified_occupation_state.up_spin_list
+        initial_down_spin_list = simplified_occupation_state.down_spin_list
+        initial_coefficient = simplified_occupation_state.coefficient
+        
+        final_up_spin_list = initial_up_spin_list
+        final_down_spin_list = initial_down_spin_list
+        final_coefficient = initial_coefficient
+        
         if self.spin == "up":
-            # CODE GOES HERE
+            skips = 0
+            if (self.spin in initial_up_spin_list):
+                # CODE GOES HERE
+            else:
+                final_coefficient = 0.0 # annihilation
         else:
-            # CODE GOES HERE
-
-
+            skips = len(initial_up_spin_list)
+            if (self.spin in initial_down_spin_list):
+                # CODE GOES HERE
+            else:
+                final_coefficient  = 0.0 # annihilation
+        
+        final_state = occupation_state.Occupation_State(final_coefficient,final_up_spin_list,final_down_spin_list) 
+        return final_state
--- creation_operator.py
+++ creation_operator.py
@@ -1,9 +1,13 @@
+import occupation_state
+
 class Creation_Operator:
-    def __init__(self,index,spin):
+    def __init__(self,numerical_index,spin):
         self.numerical_index = numerical_index
         self.spin = spin
     def apply(self,simplified_occupation_state):
         if self.spin == "up":
+            pass
             # CODE GOES HERE
         else:
+            pass
             # CODE GOES HERE
